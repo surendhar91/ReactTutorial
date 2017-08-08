b22e787e26f24ffce3e301b817c3db3f3d5ab000
Sundaresan <susurend@amazon.com>
2017-08-08 18:21:06 +0530

    html webpack plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',//inject the js below the body tag (script tag will be added)
+  inject: 'body'
+});
 module.exports = {
   entry: {
   	index: './client/index.js'
   },
   output: {
-    path: path.resolve('dist/react'),
-    filename: '[name]_bundle.js',
-    publicPath: '/react'
+    path: path.resolve('dist'),
+    filename: '[name]_bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
- }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
 
