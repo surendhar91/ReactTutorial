3c365ca221f7f2ca3b6442f4d860fe1c2834c7d4
Sundaresan <susurend@amazon.com>
2017-08-09 11:54:44 +0530

    functional component added

--- client/index.js
+++ client/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-function tick(){
-const element = (<h1>It is {new Date().toLocaleTimeString()}.</h1>);
-ReactDOM.render(element, document.getElementById('root'));
+//components are like javascript functions accept arbitrary inputs (called props) and return react elements describing what should appear on the screen.
+function Welcome(props){
+    return <h2>Hello, {props.name}</h2>;
 }
-setInterval(tick,1000);
+
+function tick() {
+    const element = (<h1>It is {new Date().toLocaleTimeString()}.</h1>);
+    ReactDOM.render(<Welcome name="surendhar"/>, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
