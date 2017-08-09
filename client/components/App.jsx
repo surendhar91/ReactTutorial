8935660fee8ee6ef42a5faaa2600edf7aec583ab
Sundaresan <susurend@amazon.com>
2017-08-09 18:00:12 +0530

    embedding any expressions in curly braces

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,12 +7,12 @@ class App extends React.Component{
 }
 function NumberList(props) {
     const numbers = props.numbers;
-    const listItems = numbers.map((number)=>(
-        //everything that's inside the map, should be given key, so that react will efficiently identify the list item added,removed or changed.
-        <ListItem key={number.toString()} value={number} />)
+    return (<div><ul>{
+        numbers.map((number)=>(
+            //everything that's inside the map, should be given key, so that react will efficiently identify the list item added,removed or changed.
+            <ListItem key={number.toString()} value={number} />)
         //if you were to return, then use curly braces.
-    );
-    return (<div><ul>{listItems}</ul></div>);
+    )}</ul></div>);
 }
 function ListItem(props){
     return (<li>{props.value}</li>);
