3f48e211698425656317c236f925acbe2c2df85a
Sundaresan <susurend@amazon.com>
2017-08-09 17:58:52 +0530

    NumberList tutorial

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,58 +1,21 @@
 import React from 'react';
 class App extends React.Component{
   render() {
-    return <LoginControl/>;
+    const numbers = [1,2,3,4,5];
+    return (<NumberList numbers={numbers}/>);
   }
 }
-class LoginControl extends React.Component{
-    //Based on state/condition, show this component. Have this logic in LoginControl
-    constructor(props){
-        super(props);
-        this.state = {isLoggedIn:false};
-        this.handleLoginAction = this.handleLoginAction.bind(this);
-        this.handleLogoutAction = this.handleLogoutAction.bind(this);
-    }
-    handleLoginAction(){
-        this.setState({isLoggedIn:true});
-    }
-
-    handleLogoutAction(){
-        this.setState({isLoggedIn:false});
-    }
-    render(){
-        let button = null;
-        let isLoggedIn = this.state.isLoggedIn;
-        if(isLoggedIn){
-            button = <LogoutButton onClick={this.handleLogoutAction}/>;//you shouldn't call the function
-            //passing the onclick action to lower level components.
-        }else{
-            button = <LoginButton onClick={this.handleLoginAction}/>;
-        }
-        return (<div><Greeting isLoggedIn={isLoggedIn}/>{button}</div>);
-    }
-
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number)=>(
+        //everything that's inside the map, should be given key, so that react will efficiently identify the list item added,removed or changed.
+        <ListItem key={number.toString()} value={number} />)
+        //if you were to return, then use curly braces.
+    );
+    return (<div><ul>{listItems}</ul></div>);
 }
-function LoginButton(props){
-    return (<button onClick={props.onClick}>Login</button>);
-}
-function LogoutButton(props){
-    return (<button onClick={props.onClick}>Logout</button>);
-}
-function Greeting(props){
-    let greeting = null;
-    if(props.isLoggedIn){
-        greeting = <UserGreeting />;
-    }else{
-        greeting = <GuestGreeting />;
-    }
-    return greeting;
-}
-//conditional components
-function UserGreeting(props){
-    return <h1>Welcome Back!</h1>;
-}
-function GuestGreeting(props){
-    return <h1>Please Sign up</h1>;
+function ListItem(props){
+    return (<li>{props.value}</li>);
 }
 export default App;
 
