a0bd1cdfc4ee9dc5bacec781bee345e28b8792a7
Sundaresan <susurend@amazon.com>
2017-08-09 11:40:49 +0530

    Rendering React elements

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick(){
+const element = (<h1>It is {new Date().toLocaleTimeString()}.</h1>);
+ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(tick,1000);
